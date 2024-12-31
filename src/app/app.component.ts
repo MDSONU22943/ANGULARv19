951fe9f5e4f0f8c2db9edfe32034bcce78896d2d
MDSONU22943 <mdsonu22943@gmail.com>
2024-12-31 18:57:19 +0530

    Function call on Button Click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
+
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent,SignupComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Code step by step';
-  name="Anil Sidhu"
-  x=10;
-  y=20
-  user1="anil"
-  user2 = "peter"
+    handleClickEvent(){
+      console.log("Function Call")
+      this.otherFunction();
+      
+    }
+    otherFunction(){
+      console.log("other function");
+      
+    }
 }
